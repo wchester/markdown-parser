32b033b30f290c9cdfcfbd7518e385a2fd5dde44
wchester <wchester@ucsd.edu>
2022-04-15 17:29:16 -0700

    FixedError1
    The error this time was that there was no link at all, so we checked to see if "[" was in the file and if it isn't just return.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,6 +11,9 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+        if(markdown.indexOf("[", currentIndex) == -1){
+            return toReturn;
+        }
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
