d9af88755b79abc9cb065c47d2c10f0546bd6b9a
wchester <wchester@ucsd.edu>
2022-04-15 17:19:28 -0700

    Real Fixed Program
    To stop the infinite loop we checked if closeParen + 1 became smaller by comparing it to currentIndex. If currentIndex continued to get bigger but closeParen + 1 went back to the start of the text file then the infinite loop was occurring. The if statement we made checks this and breaks the loop.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,7 +16,7 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if((closeParen + 1) < currentIndex){
+            if((closeParen + 1) < currentIndex) {
                 break;
             }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
